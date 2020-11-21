e042d0ed7195f0401eb11dbc62118d1aba0edc9e
Keith Smiley <keithbsmiley@gmail.com>
2020-11-20 22:46:02 -0800

    Bump to 1.1.0

--- modmove.rb
+++ modmove.rb
@@ -1,6 +1,6 @@
 cask 'modmove' do
-  version '1.0.1'
-  sha256 'e26b49608d57659e4a7a22b3e48a10d6636d79022240ea555357ef9ff323f0c9'
+  version '1.1.0'
+  sha256 '7e6890beeea2a4e2a19656b6e09986b1a77b044f179bb62a59cff05fec4b31f9'
 
   url "https://github.com/keith/modmove/releases/download/#{version}/ModMove.app.zip"
   appcast 'https://github.com/keith/modmove/releases.atom'
